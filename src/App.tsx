f5b8e24ae01f48cb4da46fb9b9447682a0fcb017
MX <notanactualemail@gmail.com>
2025-04-25 14:33:18 +0200

    added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,72 @@
 import { useState } from "react";
-import "./tailwind.css"
+import "./tailwind.css";
 
 function App() {
-	const [input, setInput] = useState("");
-	const [answer, setAnswer] = useState("");
-	const [reasoning, setReasoning] = useState("");
-	const [status, setStatus] = useState<"idle" | "fetching" | "fetched">("idle");
-	let displayAnswer, displayReason;
+    const [input, setInput] = useState("");
+    const [answer, setAnswer] = useState("");
+    const [reasoning, setReasoning] = useState("");
+    const [status, setStatus] = useState<"idle" | "fetching" | "fetched">(
+        "idle",
+    );
+    let displayAnswer, displayReason;
 
-	const makeRequest = async() => {
-		if(status === "fetching" || input.trim() === "") return;
-		setStatus("fetching");
-		const response = await fetch('https://openrouter.ai/api/v1/chat/completions', {
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				"model": "microsoft/mai-ds-r1:free",
-				"messages": [
-				  {
-					"role": "user",
-					"content": input
-				  }
-				]
-			}),
-		});
+    const makeRequest = async () => {
+        if (status === "fetching" || input.trim() === "") return;
+        setStatus("fetching");
+        const response = await fetch(
+            "https://openrouter.ai/api/v1/chat/completions",
+            {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    model: "microsoft/mai-ds-r1:free",
+                    messages: [
+                        {
+                            role: "user",
+                            content: input,
+                        },
+                    ],
+                }),
+            },
+        );
 
-		const data = await response.json();
-		console.log("RESPONSE:", data);
-		setAnswer(data.choices[0].message.content);
-		setReasoning(data.choices[0].message.reasoning);
-		setStatus("fetched");
-	}
+        const data = await response.json();
+        console.log("RESPONSE:", data);
+        setAnswer(data.choices[0].message.content);
+        setReasoning(data.choices[0].message.reasoning);
+        setStatus("fetched");
+    };
 
-	if(status === "idle"){
-		displayAnswer = "Make a request";
-		displayReason = "Make a request";
-	}else if(status === "fetching"){
-		displayAnswer = "Loading answer";
-		displayReason = "loading reason";
-	}else {
-		displayAnswer = answer;
-		displayReason = reasoning;
-	}
+    if (status === "idle") {
+        displayAnswer = "Make a request";
+        displayReason = "Make a request";
+    } else if (status === "fetching") {
+        displayAnswer = "Loading answer";
+        displayReason = "loading reason";
+    } else {
+        displayAnswer = answer;
+        displayReason = reasoning;
+    }
 
-	return (
-		<>
-			<input value={input} onChange={e => setInput(e.target.value)}/>
-			<button disabled={status === "fetching"} onClick={() => makeRequest()}>
-				Send request
-			</button>
-			<hr/>
-			<div>{displayAnswer}</div>
-			<hr className="border-dashed border-b-black"/>
-			<div>{displayReason}</div>
-		</>
-	)
+    return (
+        <>
+            <input value={input} onChange={(e) => setInput(e.target.value)} />
+            <button
+                disabled={status === "fetching"}
+                onClick={() => makeRequest()}
+                className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+            >
+                Send request
+            </button>
+            <hr />
+            <div>{displayAnswer}</div>
+            <hr className="border-dashed border-b-black" />
+            <div>{displayReason}</div>
+        </>
+    );
 }
 
 export default App;
