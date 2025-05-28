c689a5cff6f63ce89d01612be63bb9249f3f326f
MX <notanactualemail@gmail.com>
2025-05-28 23:20:22 +0200

    added immer

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { nanoid } from "nanoid";
+import { immer } from "zustand/middleware/immer";
 
 import { Roles, Chats, LoadingStatuses } from "./types/chat";
 
@@ -9,50 +10,33 @@ interface StoreState {
   setStatus: (chatID: string, newStatus: LoadingStatuses) => void;
 }
 
-const useChatsStore = create<StoreState>((set) => ({
-  chats: {
-    allChats: {
-      "V1StGXR8_Z5jdHi6B-myT": {
-        status: "idle",
-        model: "meta-llama/llama-4-scout:free",
-        messages: [],
-        startDate: new Date(),
+const useChatsStore = create<StoreState>()(
+  immer((set) => ({
+    chats: {
+      allChats: {
+        "V1StGXR8_Z5jdHi6B-myT": {
+          status: "idle",
+          model: "meta-llama/llama-4-scout:free",
+          messages: [],
+          startDate: new Date(),
+        },
       },
+      currentChatId: "V1StGXR8_Z5jdHi6B-myT",
     },
-    currentChatId: "V1StGXR8_Z5jdHi6B-myT",
-  },
-  addMessage: (chatId, role, message) =>
-    set((state) => ({
-      ...state,
-      chats: {
-        ...state.chats,
-        allChats: {
-          ...state.chats.allChats,
-          [chatId]: {
-            ...state.chats.allChats[chatId],
-            messages: [
-              ...state.chats.allChats[chatId].messages,
-              { role: role, content: message, id: nanoid() },
-            ],
-          },
-        },
-      },
-    })),
-  setStatus: (chatId, newStatus) =>
-    set((state) => ({
-      ...state,
-      chats: {
-        ...state.chats,
-        allChats: {
-          ...state.chats.allChats,
-          [chatId]: {
-            ...state.chats.allChats[chatId],
-            status: newStatus,
-          },
-        },
-      },
-    })),
-}));
+    addMessage: (chatId, role, message) =>
+      set((state) => {
+        state.chats.allChats[chatId].messages.push({
+          role: role,
+          content: message,
+          id: nanoid(),
+        });
+      }),
+    setStatus: (chatId, newStatus) =>
+      set((state) => {
+        state.chats.allChats[chatId].status = newStatus;
+      }),
+  })),
+);
 
 export const useCurrentChat = () =>
   useChatsStore((state) => state.chats.allChats[state.chats.currentChatId]);
