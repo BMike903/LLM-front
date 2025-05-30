c2fd30f34d9ac7003bc76308b139c67ab6c752da
MX <notanactualemail@gmail.com>
2025-05-30 19:07:08 +0200

    previews for chats

--- src/components/chatList.tsx
+++ src/components/chatList.tsx
@@ -2,7 +2,13 @@ import useChatsStore from "../store";
 
 function ChatList() {
   const allChats = useChatsStore((state) => state.chats.allChats);
-  const chatIDs = Object.keys(allChats);
+
+  const ChatsPreview = Object.entries(allChats).map(
+    ([chatID, chatContent]) => ({
+      chatID,
+      firstMessage: chatContent.messages[0].content,
+    }),
+  );
 
   return (
     <div
@@ -10,12 +16,12 @@ function ChatList() {
       className="flex h-full border-4 border-solid border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-black"
     >
       <ul className="flex flex-col gap-6 px-1 py-6">
-        {chatIDs.map((id) => (
+        {ChatsPreview.map(({ chatID, firstMessage }) => (
           <li
-            key={id}
+            key={chatID}
             className="rounded-md border-2 border-solid border-gray-300 bg-gray-200 p-1 hover:cursor-pointer hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-700"
           >
-            {id}
+            {firstMessage}
           </li>
         ))}
       </ul>
