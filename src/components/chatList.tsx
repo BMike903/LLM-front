e34c6a888ce2829a1b1c4fa99565adaa1b9d7a54
MX <notanactualemail@gmail.com>
2025-05-31 17:53:25 +0200

    markup and styles for add button

--- src/components/chatList.tsx
+++ src/components/chatList.tsx
@@ -16,10 +16,16 @@ function ChatList() {
   return (
     <div
       id="chatList"
-      className="flex h-full flex-1/6 border-4 border-solid border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-black"
+      className="flex h-full flex-1/6 flex-col border-4 border-solid border-gray-300 bg-gray-50 px-1 py-6 dark:border-gray-600 dark:bg-black"
     >
-      <ul className="flex flex-col gap-6 px-1 py-6">
-        <li onClick={() => addNewChat()}>Add new chat</li>
+      <div
+        className="rounded-md border-2 border-solid border-gray-300 bg-gray-200 p-1 text-center hover:cursor-pointer hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-700"
+        onClick={() => addNewChat()}
+      >
+        Add new chat
+      </div>
+      <hr className="my-5" />
+      <ul className="flex flex-col gap-6 px-1">
         {ChatsPreview.map(({ chatID, firstMessage }) => (
           <li
             key={chatID}
