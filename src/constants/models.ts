2bbdeaa04a4720c5755cb32984f391ef3bd57a6c
MX <notanactualemail@gmail.com>
2025-06-21 13:44:36 +0200

    ds-r1 added

--- src/constants/models.ts
+++ src/constants/models.ts
@@ -13,4 +13,10 @@ export const models: ModelsList = {
     description:
       "MAI-DS-R1 is a DeepSeek-R1 reasoning model that has been post-trained by the Microsoft AI team to improve its responsiveness on blocked topics and its risk profile, while maintaining its reasoning capabilities and competitive performance. ",
   },
+  "ds-r1": {
+    name: "DS R1",
+    APIName: "deepseek/deepseek-r1-0528:free",
+    description:
+      "DeepSeek-R1 is a large language model developed by DeepSeek, an AI research company. The model is designed to assist with tasks such as text generation, language understanding, problem-solving, and document analysis. ",
+  },
 } as const;
