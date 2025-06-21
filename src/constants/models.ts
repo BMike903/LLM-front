4ba7615a59a37d469f8ee64e3b385fe9f403de11
MX <notanactualemail@gmail.com>
2025-06-21 13:54:11 +0200

    gemma added

--- src/constants/models.ts
+++ src/constants/models.ts
@@ -8,15 +8,21 @@ export const models: ModelsList = {
       "Llama is a Meta-designed conversational model that adapts to a wide range of interaction styles. With a focus on flexibility and responsiveness, Llama provides quick and accurate answers, engages in in-depth discussions, and facilitates creative exchanges, making it a versatile tool for various conversational needs.",
   },
   "mai-ds": {
-    name: "MAI-DS R1",
+    name: "Microsoft AI DS-R1",
     APIName: "microsoft/mai-ds-r1:free",
     description:
       "MAI-DS-R1 is a DeepSeek-R1 reasoning model that has been post-trained by the Microsoft AI team to improve its responsiveness on blocked topics and its risk profile, while maintaining its reasoning capabilities and competitive performance. ",
   },
   "ds-r1": {
-    name: "DS R1",
+    name: "Deepseek R1",
     APIName: "deepseek/deepseek-r1-0528:free",
     description:
       "DeepSeek-R1 is a large language model developed by DeepSeek, an AI research company. The model is designed to assist with tasks such as text generation, language understanding, problem-solving, and document analysis. ",
+  },
+  "gemma-3n": {
+    name: "Google Gemma",
+    APIName: "google/gemma-3n-e4b-it:free",
+    description:
+      "Gemma is a large language model created by Google DeepMind and released as an open-weights AI assistant. It processes both text and images to generate text-based responses, operating without access to external tools or real-time information.",
   },
 } as const;
