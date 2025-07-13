89ce51a31c7348272910da849ccbe7dcc9434570
MX <notanactualemail@gmail.com>
2025-07-14 00:32:50 +0200

    removed x- prefix from custom token

--- src/services/chatService.ts
+++ src/services/chatService.ts
@@ -20,7 +20,7 @@ export async function sendMessage(message: string, currentChatId: string) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "X-Client-Token": "publictoken",
+      "Client-Token": "publictoken",
     },
     body: JSON.stringify({
       model: model.APIName,
@@ -62,7 +62,7 @@ export async function suggestTitle(chatID: string) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "X-Client-Token": "publictoken",
+      "Client-Token": "publictoken",
     },
     body: JSON.stringify({
       model: model.APIName,
