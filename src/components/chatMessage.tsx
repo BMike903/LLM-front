8fde8f83a0e422076d825694f7d87c54896de2ae
MX <notanactualemail@gmail.com>
2025-09-26 22:02:02 +0200

    modal confirmation on message delete

--- src/components/chatMessage.tsx
+++ src/components/chatMessage.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Markdown from "react-markdown";
 import { FiTrash2 } from "react-icons/fi";
 
@@ -5,20 +7,25 @@ import FileItem from "./fileItem";
 import { Message } from "../types/chat";
 import useChatsStore from "../store/store";
 import { useCurrentChatId } from "../store/chatSelectors";
+import ConfirmModal from "./ConfirmModal";
 
 function ChatMessage({ message }: { message: Message }) {
   const deleteMessage = useChatsStore((state) => state.deleteMessage);
   const currentChatId = useCurrentChatId();
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  let messageItem;
+
   if (message.role === "user") {
-    return (
+    messageItem = (
       <div key={message.id} className="group flex w-full justify-end">
         <div className="relative max-w-[80%]">
           <button
             className="absolute top-1 -left-6 text-gray-500 opacity-0 transition-opacity group-hover:opacity-100 hover:cursor-pointer hover:text-gray-950 dark:text-gray-300 dark:hover:text-gray-50"
             onClick={(e) => {
               e.stopPropagation();
-              if (currentChatId) deleteMessage(currentChatId, message.id);
+              setIsModalOpen(true);
             }}
             aria-label="Delete message"
           >
@@ -43,13 +50,13 @@ function ChatMessage({ message }: { message: Message }) {
       </div>
     );
   } else {
-    return (
+    messageItem = (
       <div className="group relative pl-10" key={message.id}>
         <button
           className="absolute top-2 left-2 text-gray-500 opacity-0 transition-opacity group-hover:opacity-100 hover:cursor-pointer hover:text-gray-950 dark:text-gray-300 dark:hover:text-gray-50"
           onClick={(e) => {
             e.stopPropagation();
-            if (currentChatId) deleteMessage(currentChatId, message.id);
+            setIsModalOpen(true);
           }}
           aria-label="Delete message"
         >
@@ -61,6 +68,18 @@ function ChatMessage({ message }: { message: Message }) {
       </div>
     );
   }
+  return (
+    <>
+      {" "}
+      {messageItem}{" "}
+      <ConfirmModal
+        title="Delete Message"
+        description="Are you sure you want to delete this message?"
+        onConfirm={() => deleteMessage(currentChatId, message.id)}
+        isOpen={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+      />{" "}
+    </>
+  );
 }
-
 export default ChatMessage;
